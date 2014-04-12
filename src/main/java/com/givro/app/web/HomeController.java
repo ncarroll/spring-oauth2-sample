d934579ab186ddd390f60b05625acfa0a56cad89
Nick Carroll <nlcarroll@gmail.com>
2014-04-12 22:03:45 +1000

    Added controllers.

---
+++ src/main/java/com/givro/app/web/HomeController.java
@@ -0,0 +1,13 @@
+package com.givro.app.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "home";
+    }
+}
